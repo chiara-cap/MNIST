83c4135cb99242ef85876ec198c5e598b57906fe
alfredoonori22 <alfredoonori45@gmail.com>
2023-05-07 22:27:25 +0200

    committ.yaml ma questa volta con la GPU che non esplode

--- training.py
+++ training.py
@@ -28,16 +28,17 @@ def train_one_epoch(epoch_index):
     last_loss = 0.0
 
     for i, data in enumerate(training_loader):
-
         # Every data instance is an input + label pair
         inputs, labels = data
-        inputs, labels = inputs.cuda(), labels.cuda()
+        inputs = inputs.cuda()
 
         # Zero your gradients for every batch!
         optimizer.zero_grad()
 
         # Make predictions for this batch
         outputs = model(inputs)
+        inputs = inputs.cpu()
+        outputs = outputs.cpu()
 
         # Compute the loss and its gradients
         loss = loss_fn(outputs, labels)
@@ -48,26 +49,11 @@ def train_one_epoch(epoch_index):
 
         # Gather data
         running_loss += loss.item()
+
         if i % 1000 == 999:
-            last_loss = running_loss / 1000 # loss per batch
+            last_loss = running_loss / 1000  # loss per batch
             print('  batch {} loss: {}'.format(i + 1, last_loss))
             running_loss = 0.0
-
-            # Validation
-            # We don't need gradients on to do reporting
-            model.train(False)
-
-            running_vloss = 0.0
-            for i, vdata in enumerate(validation_loader):
-                vinputs, vlabels = vdata
-                vinputs, vlabels = vinputs.cuda(), vlabels.cuda()
-                voutputs = model(vinputs)
-                vloss = loss_fn(voutputs, vlabels)
-                running_vloss += vloss
-
-            avg_vloss = running_vloss / (i + 1)
-            print('LOSS train {} valid {}'.format(last_loss, avg_vloss))
-            wandb.log({'train-loss': last_loss, 'valid-loss': avg_vloss})
 
     return last_loss
 
@@ -97,12 +83,10 @@ print('Training set has {} instances'.format(len(training_set)))
 print('Validation set has {} instances'.format(len(validation_set)))
 print('Test set has {} instances'.format(len(test_set)))
 
-
 # Defining the model
 model = models.resnet18()
 model.conv1 = torch.nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
 model = model.cuda()
-
 
 # Defining Loss Function
 loss_fn = torch.nn.CrossEntropyLoss()
@@ -110,13 +94,12 @@ loss_fn = torch.nn.CrossEntropyLoss()
 # Optimizers specified in the torch.optim package
 optimizer = torch.optim.SGD(model.parameters(), lr=0.001, momentum=0.9)
 
-
 # Initializing in a separate cell, so we can easily add more epochs to the same run
 timestamp = datetime.now().strftime('%Y%m%d_%H%M%S')
 epoch_number = 0
 
-EPOCHS = 2
-best_test_loss = 1_000_000.
+EPOCHS = 5
+best_vloss = 1_000_000.
 
 for epoch in range(EPOCHS):
     print('EPOCH {}:'.format(epoch_number + 1))
@@ -125,27 +108,55 @@ for epoch in range(EPOCHS):
     model.train(True)
     avg_loss = train_one_epoch(epoch_number)
 
+    # Validation
     # We don't need gradients on to do reporting
     model.train(False)
+    with torch.no_grad():
+        running_vloss = 0.0
+        i = 0
+        for i, vdata in enumerate(validation_loader):
+            vinputs, vlabels = vdata
+            vinputs = vinputs.cuda()
 
-    running_test_loss = 0.0
-    for i, test_data in enumerate(test_loader):
-        test_inputs, test_labels = test_data
-        test_inputs, test_labels = test_inputs.cuda(), test_labels.cuda()
-        test_outputs = model(test_inputs)
-        test_loss = loss_fn(test_outputs, test_labels)
-        running_test_loss += test_loss
+            voutputs = model(vinputs)
+            vinputs = vinputs.cpu()
+            voutputs = voutputs.cpu()
 
-    avg_test_loss = running_test_loss / (i + 1)
-    print('LOSS train {} test {}'.format(avg_loss, avg_test_loss))
+            vloss = loss_fn(voutputs, vlabels)
+            running_vloss += vloss
+
+        avg_vloss = running_vloss / (i + 1)
 
     # Track best performance, and save the model's state
-    if avg_test_loss < best_test_loss:
-        best_test_loss = avg_test_loss
+    if avg_vloss < best_vloss:
+        best_vloss = avg_vloss
         model_path = 'model_{}_{}'.format(timestamp, epoch_number)
         torch.save(model.state_dict(), model_path)
 
+    print('LOSS valid {}'.format(avg_vloss))
+    wandb.log({'train-loss': avg_loss, 'valid-loss': avg_vloss})
+
     epoch_number += 1
+
+# We don't need gradients on to do reporting
+model.train(False)
+
+with torch.no_grad():
+    running_test_loss = 0.0
+    i = 0
+    for i, test_data in enumerate(test_loader):
+        test_inputs, test_labels = test_data
+        test_inputs = test_inputs.cuda()
+
+        test_outputs = model(test_inputs)
+
+        test_outputs = test_outputs.cpu()
+        test_loss = loss_fn(test_outputs, test_labels)   #ci va l'accuracy non la loss
+
+        running_test_loss += test_loss
+
+avg_test_loss = running_test_loss / (i + 1)
+print('LOSS train {} test {}'.format(avg_loss, avg_test_loss))
 
 
 # Close wandb connection
